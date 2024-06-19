bd770c97452c3b30452cd8b0aa147629a53cabba
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-06-19 19:22:27 +0200

    Added: container-md and deleted comment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,9 +23,8 @@
 </div>
 
 <div class="content">
-    <div class="container">
+    <div class="container-md">
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p>
     </div>
 </div>
-{{-- MODIFICA RACHEL PROVA --}}
 @endsection
